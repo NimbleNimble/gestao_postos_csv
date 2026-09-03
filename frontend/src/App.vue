b4ade6200281ec820627991716d1560212800cd4
dev@Nimble <dev@nimblenimble.com>
2026-09-03 19:20:28 -0300

    add @mdi/font dependency

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,10 @@
     <v-main>
       <v-container>
         <v-card class="pa-4" elevation="2">
-          <v-card-title>Bem-vindo</v-card-title>
+          <v-card-title>
+            <span class="mdi mdi-gas-station"></span>
+            Bem-vindo
+          </v-card-title>
           <v-card-text>Vue 3 + Vuetify rodando com sucesso.</v-card-text>
         </v-card>
       </v-container>
