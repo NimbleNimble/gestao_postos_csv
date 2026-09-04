57de58d567b855f671b0a7a18869262ec9df2686
dev@Nimble <dev@nimblenimble.com>
2026-09-03 22:04:29 -0300

    feat(frontend): - add file-upload (drag & drop) component - add data-table component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,23 @@
     <v-app-bar color="primary" title="Gestão de Postos" />
     <v-main>
       <v-container>
+        <v-file-upload class="mt-7" density="comfortable"  variant="comfortable" title="Importar arquivo"></v-file-upload>
+      </v-container>
+      <v-container>
         <v-card class="pa-4" elevation="2">
           <v-card-title>
             <span class="mdi mdi-gas-station"></span>
-            Bem-vindo
+            Listagem de postos
           </v-card-title>
-          <v-card-text>Vue 3 + Vuetify rodando com sucesso.</v-card-text>
+          <v-card-text>
+            <v-data-table-virtual
+                :headers="mock_headers"
+                :items="mock_data"
+                height="400"
+                item-value="name"
+                fixed-header
+            ></v-data-table-virtual>
+          </v-card-text>
         </v-card>
       </v-container>
     </v-main>
@@ -16,4 +27,84 @@
 </template>
 
 <script setup>
+const mock_headers = [
+    { title: 'Boat Type', align: 'start', key: 'name' },
+    { title: 'Speed(knots)', align: 'end', key: 'speed' },
+    { title: 'Length(m)', align: 'end', key: 'length' },
+    { title: 'Price($)', align: 'end', key: 'price' },
+    { title: 'Year', align: 'end', key: 'year' },
+]
+
+const mock_data = [
+    {
+    name: 'Speedster',
+    speed: 35,
+    length: 22,
+    price: 300000,
+    year: 2021,
+    },
+    {
+    name: 'OceanMaster',
+    speed: 25,
+    length: 35,
+    price: 500000,
+    year: 2020,
+    },
+    {
+    name: 'Voyager',
+    speed: 20,
+    length: 45,
+    price: 700000,
+    year: 2019,
+    },
+    {
+    name: 'WaveRunner',
+    speed: 40,
+    length: 19,
+    price: 250000,
+    year: 2022,
+    },
+    {
+    name: 'SeaBreeze',
+    speed: 28,
+    length: 31,
+    price: 450000,
+    year: 2018,
+    },
+    {
+    name: 'HarborGuard',
+    speed: 18,
+    length: 50,
+    price: 800000,
+    year: 2017,
+    },
+    {
+    name: 'SlickFin',
+    speed: 33,
+    length: 24,
+    price: 350000,
+    year: 2021,
+    },
+    {
+    name: 'StormBreaker',
+    speed: 22,
+    length: 38,
+    price: 600000,
+    year: 2020,
+    },
+    {
+    name: 'WindSail',
+    speed: 15,
+    length: 55,
+    price: 900000,
+    year: 2019,
+    },
+    {
+    name: 'FastTide',
+    speed: 37,
+    length: 20,
+    price: 280000,
+    year: 2022,
+    },
+]
 </script>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,5 +1,8 @@
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import { VFileUpload } from 'vuetify/labs/VFileUpload'
 
-export default createVuetify()
+export default createVuetify({
+  components: { VFileUpload },
+})
