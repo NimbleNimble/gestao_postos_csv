26dd79577ca09fc94c1268fd80095064d72f5acf
dev@Nimble <dev@nimblenimble.com>
2026-09-05 18:51:49 -0300

    feat(migrations): add bandeiras table migration

---
+++ backend/migrations/1788643359685_bandeiras.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("bandeiras", {
+    id: "id",
+    nome: { type: "varchar(255)", notNull: true, unique: true },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("bandeiras");
+};
