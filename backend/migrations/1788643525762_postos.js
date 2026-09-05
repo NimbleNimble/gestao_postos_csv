4d1ac311f3f9aa00b84a6dcd36d97211e3ac12b1
dev@Nimble <dev@nimblenimble.com>
2026-09-05 19:07:14 -0300

    feat(migrations): add foreign key constraints to postos table

--- backend/migrations/1788643525762_postos.js
+++ backend/migrations/1788643525762_postos.js
@@ -11,10 +11,30 @@ export const shorthands = undefined;
 export const up = (pgm) => {
   pgm.createTable("postos", {
     id: "id",
-    responsavel_id: { type: "integer", notNull: true },
-    bandeira_id: { type: "integer", notNull: true },
-    municipio_id: { type: "integer", notNull: true },
-    status_id: { type: "integer", notNull: true },
+    responsavel_id: {
+      type: "integer",
+      notNull: true,
+      references: '"responsaveis"',
+      onDelete: "RESTRICT",
+    },
+    bandeira_id: {
+      type: "integer",
+      notNull: true,
+      references: '"bandeiras"',
+      onDelete: "RESTRICT",
+    },
+    municipio_id: {
+      type: "integer",
+      notNull: true,
+      references: '"municipios"',
+      onDelete: "RESTRICT",
+    },
+    status_id: {
+      type: "integer",
+      notNull: true,
+      references: '"status"',
+      onDelete: "RESTRICT",
+    },
     cnpj: { type: "char(14)", notNull: true, unique: true },
     nome: { type: "varchar(255)", notNull: true, unique: true },
     nome_fantasia: { type: "varchar(255)" },
