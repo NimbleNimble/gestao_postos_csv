bef1b1a9c2a948a357831b58a7c567f485f80d36
dev@Nimble <dev@nimblenimble.com>
2026-09-06 17:45:47 -0300

    feat(front): update GestaoPostosUploadDialogComponent

--- frontend/src/components/GestaoPostosUploadDialogComponent.vue
+++ frontend/src/components/GestaoPostosUploadDialogComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <v-dialog transition="dialog-bottom-transition" width="auto">
+        <v-dialog transition="dialog-bottom-transition" fullscreen>
             <template v-slot:activator="{ props: activatorProps }">
                 <v-btn title="Importe o relatório em arquivo CSV para atualizar a base de dados" v-bind="activatorProps"
                     text="Importe o relatório em arquivo CSV para atualizar a base de dados" color="red-darken-4"
@@ -13,25 +13,41 @@
 
             <template v-slot:default="{ isActive }">
                 <v-card>
-                    <v-toolbar title="Importe o relatório em arquivo CSV para atualizar a base de dados"></v-toolbar>
+                    <v-toolbar color="red-darken-4" title="Importr arquivo CSV">
+                        <v-spacer></v-spacer>
+                        <v-btn icon @click="isActive.value = false">
+                            <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                    </v-toolbar>
 
                     <v-card-text class="">
                         <!-- TODO: Melhorar texto e modal -->
-                        <p>Loren ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                        <p class="text-block text-subtitle-1">
+                            Loren ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                            eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.
+                            Loren ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                            eiusmod tempor
                             incididunt ut labore et dolore magna aliqua.
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                             ex ea commodo consequat.
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                             fugiat nulla pariatur.
                         </p>
-                        <v-file-upload v-model="file" class="mt-7" density="comfortable" variant="comfortable"
-                            title="Importar arquivo"></v-file-upload>
+                        <v-file-upload v-model="file" class="mt-7" density="default" title="Arraste e solte"
+                            browse-text="Selecione arquivo" divider-text="ou"></v-file-upload>
                     </v-card-text>
 
                     <v-card-actions class="justify-end">
-                        <v-btn text="Cancelar" @click="isActive.value = false" />
+                        <v-btn class="px-6" variant="outlined" color="red-darken-2" text="Cancelar"
+                            @click="isActive.value = false" />
 
-                        <v-btn text="Enviar" variant="flat" color="text-yellow"
+                        <!-- TODO: Pensar em outras cores para confirmar -->
+                        <v-btn class="px-6" variant="flat" color="red-darken-2" text="Enviar"
                             :disabled="!file || !file.name.endsWith('.csv')"
                             @click="uploadFile(file); isActive.value = false" />
                     </v-card-actions>
