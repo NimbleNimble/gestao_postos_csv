0b7dad227f3be534283a82ad76ac300e56a9ba2c
dev@Nimble <dev@nimblenimble.com>
2026-09-06 11:42:57 -0300

    fix(backend): refactor list SQL query

--- backend/controllers/listController.js
+++ backend/controllers/listController.js
@@ -1,16 +1,48 @@
 const pool = require("../db/pool");
 
 const listController = async (req, res) => {
-  // TODO: Continuar aqui
   try {
     const result = await pool.query(
-      `SELECT p.cnpj, p.nome as nome_posto, p.nome_fantasia, b.nome as bandeira, p.logradouro, p.numero, p.complemento, p.bairro, m.nome as municipio, m.uf, p.cep, r.cpf as cpf_responsavel, r.nome as nome_responsavel, r.email as email_responsavel, r.cargo as cargo_responsavel, (SELECT STRING_AGG(c.nome, ', ' ORDER BY c.nome DESC) FROM postos_combustiveis pc
-join combustiveis c on c.id = pc.combustivel_id 
- where pc.posto_id = p.id) as combustiveis, s.nome as status, p.data_inauguracao, p.numero_bicos, p.numero_pistas, p.observacoes  from postos p
-join responsaveis r ON r.id = p.responsavel_id  
-join bandeiras b on b.id = p.bandeira_id 
-join municipios m on m.id = p.municipio_id 
-join status s on s.id = p.status_id`,
+      `SELECT 
+            p.cnpj, 
+            p.nome AS nome_posto, 
+            p.nome_fantasia, 
+            b.nome AS bandeira, 
+            p.logradouro, 
+            p.numero, 
+            p.complemento, 
+            p.bairro, 
+            m.nome AS municipio, 
+            m.uf, 
+            p.cep, 
+            r.cpf AS cpf_responsavel, 
+            r.nome AS nome_responsavel, 
+            r.email AS email_responsavel, 
+            r.cargo AS cargo_responsavel, 
+            COALESCE(STRING_AGG(c.nome, ', ' ORDER BY c.nome DESC), '') AS combustiveis, 
+            s.nome AS status, 
+            p.data_inauguracao, 
+            p.numero_bicos, 
+            p.numero_pistas, 
+            p.observacoes 
+        FROM postos p
+        LEFT JOIN responsaveis r ON r.id = p.responsavel_id 
+        LEFT JOIN bandeiras b ON b.id = p.bandeira_id 
+        LEFT JOIN municipios m ON m.id = p.municipio_id 
+        LEFT JOIN status s ON s.id = p.status_id
+        LEFT JOIN postos_combustiveis pc ON pc.posto_id = p.id
+        LEFT JOIN combustiveis c ON c.id = pc.combustivel_id
+        GROUP BY 
+            p.id, 
+            b.nome, 
+            m.nome, 
+            m.uf, 
+            r.cpf, 
+            r.nome, 
+            r.email, 
+            r.cargo, 
+            s.nome;
+      `,
     );
     res.json({ status: "ok", data: result.rows });
   } catch (err) {
