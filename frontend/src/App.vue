3534a4306869bbfdebbd7c43d6668faf9f1b6d4c
dev@Nimble <dev@nimblenimble.com>
2026-09-06 17:05:27 -0300

    feat(front): create GestaoPostosTableComponent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,8 +9,7 @@
             prepend-icon="mdi-download" class="font-weight-bold" @click="downloadCsv" />
         </div>
         <v-divider class="my-6"></v-divider>
-        <v-data-table-virtual :headers="dataHeaders" :items="dataContent" height="400" item-value="cnpj"
-          fixed-header></v-data-table-virtual>
+        <GestaoPostosTableComponent />
       </v-container>
 
     </v-main>
@@ -18,44 +17,12 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { getData } from './services/dataService'
 import GestaoPostosHeaderComponent from './components/GestaoPostosHeaderComponent.vue'
+import GestaoPostosTableComponent from './components/GestaoPostosTableComponent.vue'
 
-const dataHeaders = [
-  { title: 'CNPJ', align: 'start', key: 'cnpj' },
-  { title: 'Nome Posto', align: 'start', key: 'nome_posto' },
-  { title: 'Nome Fantasia', align: 'start', key: 'nome_fantasia' },
-  { title: 'Bandeira', align: 'start', key: 'bandeira' },
-  { title: 'Logradouro', align: 'start', key: 'logradouro' },
-  { title: 'Número', align: 'start', key: 'numero' },
-  { title: 'Complemento', align: 'start', key: 'complemento' },
-  { title: 'Bairro', align: 'start', key: 'bairro' },
-  { title: 'Município', align: 'start', key: 'municipio' },
-  { title: 'UF', align: 'start', key: 'uf' },
-  { title: 'CEP', align: 'start', key: 'cep' },
-  { title: 'CPF Responsável', align: 'start', key: 'cpf_responsavel' },
-  { title: 'Responsável', align: 'start', key: 'nome_responsavel' },
-  { title: 'Email Responsável', align: 'start', key: 'email_responsavel' },
-  { title: 'Cargo Responsável', align: 'start', key: 'cargo_responsavel' },
-  { title: 'Combustíveis', align: 'start', key: 'combustiveis' },
-  { title: 'Status', align: 'start', key: 'status' },
-  { title: 'Data da inauguração', align: 'start', key: 'data_inauguracao' },
-  { title: 'Número de Bicos', align: 'start', key: 'numero_bicos' },
-  { title: 'Número de Pistas', align: 'start', key: 'numero_pistas' },
-  { title: 'Observações', align: 'start', key: 'observacoes' }
-]
-
-const dataContent = ref([])
 
 function downloadCsv() {
   // TODO: Revisar/Aprimorar isto
   window.location.href = "http://localhost:3000/list/export";
 }
-
-onMounted(() => {
-  getData().then(data => {
-    dataContent.value = data.data
-  })
-});
 </script>
---
+++ frontend/src/components/GestaoPostosTableComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-data-table-virtual :headers="dataHeaders" :items="dataContent" height="400" item-value="cnpj"
+        fixed-header></v-data-table-virtual>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getData } from '../services/dataService'
+
+const dataHeaders = [
+    { title: 'CNPJ', align: 'start', key: 'cnpj' },
+    { title: 'Nome Posto', align: 'start', key: 'nome_posto' },
+    { title: 'Nome Fantasia', align: 'start', key: 'nome_fantasia' },
+    { title: 'Bandeira', align: 'start', key: 'bandeira' },
+    { title: 'Logradouro', align: 'start', key: 'logradouro' },
+    { title: 'Número', align: 'start', key: 'numero' },
+    { title: 'Complemento', align: 'start', key: 'complemento' },
+    { title: 'Bairro', align: 'start', key: 'bairro' },
+    { title: 'Município', align: 'start', key: 'municipio' },
+    { title: 'UF', align: 'start', key: 'uf' },
+    { title: 'CEP', align: 'start', key: 'cep' },
+    { title: 'CPF Responsável', align: 'start', key: 'cpf_responsavel' },
+    { title: 'Responsável', align: 'start', key: 'nome_responsavel' },
+    { title: 'Email Responsável', align: 'start', key: 'email_responsavel' },
+    { title: 'Cargo Responsável', align: 'start', key: 'cargo_responsavel' },
+    { title: 'Combustíveis', align: 'start', key: 'combustiveis' },
+    { title: 'Status', align: 'start', key: 'status' },
+    { title: 'Data da inauguração', align: 'start', key: 'data_inauguracao' },
+    { title: 'Número de Bicos', align: 'start', key: 'numero_bicos' },
+    { title: 'Número de Pistas', align: 'start', key: 'numero_pistas' },
+    { title: 'Observações', align: 'start', key: 'observacoes' }
+]
+
+const dataContent = ref([])
+
+onMounted(() => {
+    getData().then(data => {
+        dataContent.value = data.data
+    })
+});
+</script>
