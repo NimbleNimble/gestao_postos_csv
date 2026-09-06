94818d380e91a6a49f80331dbfb9912e1819fb90
dev@Nimble <dev@nimblenimble.com>
2026-09-05 21:18:28 -0300

    refactor(upload): uploadController

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,9 +1,23 @@
 const uploadController = (req, res) => {
-  const isEmptyFile = !req.file || req.file.size === 0;
-  const isCsvFile = req.file && req.file.originalname.endsWith(".csv");
-  const isValid = () => !isEmptyFile && isCsvFile;
+  validateFile(req, res);
 
-  if (!isValid()) {
+  const data = convertCsvToJson(req);
+
+  res.json({
+    status: "ok",
+    filename: req.file.originalname,
+    size: req.file.size,
+    dataCount: data?.length || 0,
+    data: data || [],
+  });
+};
+
+const validateFile = (req, res) => {
+  const isEmptyFile = !req.file || req.file.size === 0;
+  const isCsvFile = req.file?.originalname.toLowerCase().endsWith(".csv");
+  const isValid = !isEmptyFile && isCsvFile;
+
+  if (!isValid) {
     const errorMessage = !isCsvFile
       ? "Extensão inválida. Arquivo enviado não é CSV"
       : "Nenhum arquivo enviado";
@@ -12,24 +26,18 @@ const uploadController = (req, res) => {
       message: errorMessage,
     });
   }
+};
 
+const convertCsvToJson = (req) => {
   const content = req.file.buffer.toString("utf-8");
   const lines = content.split(/\r?\n/).filter(Boolean);
   const headers = lines[0].split(";");
-  const data = lines.slice(1).map((line) => {
+  return lines.slice(1).map((line) => {
     const values = line.split(";");
     return headers.reduce((acc, header, index) => {
       acc[header] = values[index];
       return acc;
     }, {});
-  });
-
-  res.json({
-    status: "ok",
-    filename: req.file.originalname,
-    size: req.file.size,
-    dataCount: data.length,
-    data: data,
   });
 };
 
