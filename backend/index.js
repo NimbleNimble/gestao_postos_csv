ef30c80c5090eb23783e634eac7cd5f9dd9fb261
dev@Nimble <dev@nimblenimble.com>
2026-09-05 21:33:27 -0300

    feat(db): create database connection pool in separate module

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
+const pool = require("./db/pool");
 const uploadRoutes = require("./routes/uploadRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use("/upload", uploadRoutes);
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
 
 app.get("/", (req, res) => {
   res.json({ status: "ok" });
