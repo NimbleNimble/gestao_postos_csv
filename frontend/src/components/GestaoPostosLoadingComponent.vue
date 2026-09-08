a74d0721fbf6b3a8d17d7551d90e6000c796fe12
dev@Nimble <dev@nimblenimble.com>
2026-09-08 12:54:45 -0300

    feat(front): add loading component

---
+++ frontend/src/components/GestaoPostosLoadingComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-dialog :model-value="props.isLoading" transition="" fullscreen class="">
+        <v-container class="align-center justify-center fill-height">
+            <v-progress-circular color="yellow-darken-2" indeterminate :size="57" :width="11"></v-progress-circular>
+        </v-container>
+    </v-dialog>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    isLoading: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
